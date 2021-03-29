2a9a4cb2c74c202e408fac31f63ab39f62978d5c
Cory Wilkerson <coryjwilkerson@gmail.com>
2021-03-29 15:57:24 -0700

    See if this works for a newly created codespace

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-install-linux-packages() {
-    echo "==========================================================="
-    echo "* Install following packages:"
-    echo "-----------------------------------------------------------"
+echo "==========================================================="
+echo "* Install following packages:"
+echo "-----------------------------------------------------------"
 
-    sudo apt-get update
-    sudo apt-get install -y curl wget tree unzip screen tmux
-    sudo apt-get install -y lsof whois traceroute 
-    sudo apt-get install -y vim
-}
-
-install-linux-packages
+apt-get update
+apt-get install -y curl wget tree unzip screen tmux
+apt-get install -y lsof whois traceroute 
+apt-get install -y vim
